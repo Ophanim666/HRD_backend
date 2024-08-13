b9e9e6053d7f38c655cd0272aa395c51e7bfb410
Ophanim666 <al.arriaza@duocuc.c>
2024-08-13 17:25:52 -0400

    Nuevos cambios

--- API/Controllers/UsuarioController.cs
+++ API/Controllers/UsuarioController.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using AutoMapper;
-using DTO;
 using DTO.Usuario;
 using Models.Entidades;
-using System.Collections.Generic;
 using Data.Repositorios;
+using System.Collections.Generic;
 using System.Threading.Tasks;
-
 
 namespace API.Controllers
 {
@@ -23,31 +21,22 @@ namespace API.Controllers
             _mapper = mapper;
         }
 
-        //[HttpGet]
-        //public ActionResult<IEnumerable<UsuarioDTO>> Get()
-        //{
-        //    var usuarios = _usuarioRepositorio.GetUsuarios();
-        //    var usuarioDTOs = _mapper.Map<IEnumerable<UsuarioDTO>>(usuarios);
-
-        //    return Ok(usuarioDTOs);
-        //}
-
         [HttpGet]
         public async Task<ActionResult<List<UsuarioDTO>>> ListAll()
         {
             var response = await _usuarioRepositorio.ListAll();
-            if (response == null)
+            if (response == null || response.Count == 0)
             {
                 return NotFound();
             }
 
-            var UsuarioDTO = _mapper.Map<List<UsuarioDTO>>(response);
-            //var UsuarioDTO = mapper.Map<List<Usuario>, List<UsuarioDTO>>(response);
-
-
-            return Ok(UsuarioDTO);
-
-            //return response;
+            var usuarioDTOs = _mapper.Map<List<UsuarioDTO>>(response);
+            return Ok(usuarioDTOs);
         }
+
+        //funcion delete esta funcion quedara como softdelete que no eliminara a los usaurios de la base de datos 
+        
+
+
     }
 }
--- Models/Entidades/Usuario.cs
+++ Models/Entidades/Usuario.cs
@@ -9,6 +9,14 @@ namespace Models.Entidades
     public class Usuario
     {
         public int Id { get; set; }
-        public String UserName { get; set; }
+        public string Rut { get; set; }
+        public string Primer_Nombre { get; set; }
+        public string Segundo_Nombre { get; set; }
+        public string Primer_Apellido { get; set; }
+        public string Segundo_Apellido { get; set; }
+        public DateTime Fecha_de_nacimiento { get; set; }
+        public string Rol { get; set; }
+        public string Especialidad { get; set; }
     }
 }
+
