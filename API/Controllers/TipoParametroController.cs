d1ce09cb4d818001469291c9a048c6e2558f50d5
Ophanim666 <al.arriaza@duocuc.c>
2024-08-15 03:42:01 -0400

    NUEVOS CAMBIOS PARA GUARDAR EN CASO DE ACCIDENTE Y HACER UN ROLLBACK

---
+++ API/Controllers/TipoParametroController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.Entidades;
+using Data.Repositories;
+using System.Threading.Tasks;
+using DTO.TipoParametro;
+using AutoMapper;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TipoParametroController : ControllerBase
+    {
+        private readonly TipoParametroRepository _tipoParametroRepositorio;
+        private readonly IMapper _mapper;
+
+        public TipoParametroController(TipoParametroRepository tipoParametroRepositorio, IMapper mapper)
+        {
+            _tipoParametroRepositorio = tipoParametroRepositorio;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InsertarTipoParametro([FromBody] TipoParametroDTO tipoParametroDTO)
+        {
+            if (tipoParametroDTO == null)
+            {
+                return BadRequest("Datos inválidos.");
+            }
+
+            // Mapeo del DTO a la entidad del modelo si es necesario
+            var tipoParametro = new TipoParametro
+            {
+                TIPO_PARAMETRO = tipoParametroDTO.TIPO_PARAMETRO,
+                ESTADO = tipoParametroDTO.ESTADO,
+                USUARIO_CREACION = tipoParametroDTO.USUARIO_CREACION,
+                FECHA_CREACION = tipoParametroDTO.FECHA_CREACION
+            };
+
+            var result = await _tipoParametroRepositorio.InsertarTipoParametro(tipoParametro);
+
+            if (result > 0)
+            {
+                return Ok("TipoParametro insertado correctamente.");
+            }
+            else
+            {
+                return StatusCode(500, "Error al insertar el TipoParametro.");
+            }
+        }
+
+        //funcion actualiar
+        [HttpPut("{id}")]
+        public async Task<IActionResult> ActualizarTipoParametro(int id, TipoParametroDTO tipoParametroDto)
+        {
+            // Mapear DTO a entidad
+            var tipoParametro = new TipoParametro
+            {
+                ID = id,
+                TIPO_PARAMETRO = tipoParametroDto.TIPO_PARAMETRO,
+                ESTADO = tipoParametroDto.ESTADO,
+                USUARIO_CREACION = tipoParametroDto.USUARIO_CREACION,
+                FECHA_CREACION = tipoParametroDto.FECHA_CREACION
+            };
+
+            var response = await _tipoParametroRepositorio.ActualizarTipoParametro(tipoParametro);
+
+            if (response > 0) // Si el número de filas afectadas es mayor a 0
+            {
+                return Ok("TipoParametro actualizado correctamente.");
+            }
+            else
+            {
+                return BadRequest("Error al actualizar el TipoParametro.");
+            }
+        }
+
+        //Funcion listar
+        [HttpGet]
+        public async Task<ActionResult<List<TipoParametroDTO>>> ListAll()
+        {
+            var response = await _tipoParametroRepositorio.ListAll();
+            if (response == null || response.Count == 0)
+            {
+                return NotFound();
+            }
+
+            var tipoParametroDTOs = _mapper.Map<List<TipoParametroDTO>>(response);
+            return Ok(tipoParametroDTOs);
+        }
+
+
+
+    }
+}
---
+++ Data/Repositories/TipoParametroRepository.cs
@@ -0,0 +1,102 @@
+﻿using Models.Entidades;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+
+namespace Data.Repositories
+{
+    public class TipoParametroRepository
+    {
+        private readonly string _connectionString;
+
+        public TipoParametroRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        // Función para insertar un nuevo TipoParametro
+        public async Task<int> InsertarTipoParametro(TipoParametro tipoParametro)
+        {
+            using (SqlConnection sql = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("InsertarTipoParametro", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    // Añadir parámetros al procedimiento almacenado
+                    cmd.Parameters.AddWithValue("@TIPO_PARAMETRO", tipoParametro.TIPO_PARAMETRO);
+                    cmd.Parameters.AddWithValue("@USUARIO_CREACION", tipoParametro.USUARIO_CREACION);
+                    cmd.Parameters.AddWithValue("@ESTADO", tipoParametro.ESTADO);
+                    cmd.Parameters.AddWithValue("@FECHA_CREACION", tipoParametro.FECHA_CREACION);
+
+                    await sql.OpenAsync();
+                    // Ejecutar el comando y obtener el número de filas afectadas
+                    return await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+
+        //funcion para actualizar
+        public async Task<int> ActualizarTipoParametro(TipoParametro tipoParametro)
+        {
+            using (SqlConnection sql = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("ActualizarTipoParametro", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@ID", tipoParametro.ID);
+                    cmd.Parameters.AddWithValue("@TIPO_PARAMETRO", tipoParametro.TIPO_PARAMETRO);
+                    cmd.Parameters.AddWithValue("@ESTADO", tipoParametro.ESTADO);
+                    cmd.Parameters.AddWithValue("@USUARIO_CREACION", tipoParametro.USUARIO_CREACION);
+                    cmd.Parameters.AddWithValue("@FECHA_CREACION", tipoParametro.FECHA_CREACION);
+
+                    await sql.OpenAsync();
+                    return await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+
+        //Funcion para listar
+        public async Task<List<TipoParametro>> ListAll()
+        {
+            using (SqlConnection sql = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("ListarTipoParametros", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    var response = new List<TipoParametro>();
+                    await sql.OpenAsync();
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            response.Add(MapToTipoParametro(reader));
+                        }
+                    }
+
+                    return response;
+                }
+            }
+        }
+
+        private TipoParametro MapToTipoParametro(SqlDataReader reader)
+        {
+            return new TipoParametro()
+            {
+                ID = reader.GetInt32(reader.GetOrdinal("ID")),
+                TIPO_PARAMETRO = reader.GetString(reader.GetOrdinal("TIPO_PARAMETRO")),
+                ESTADO = reader.GetInt32(reader.GetOrdinal("ESTADO")),
+                USUARIO_CREACION = reader.GetString(reader.GetOrdinal("USUARIO_CREACION")),
+                FECHA_CREACION = reader.GetDateTime(reader.GetOrdinal("FECHA_CREACION"))
+            };
+        }
+
+
+    }
+
+}
+
