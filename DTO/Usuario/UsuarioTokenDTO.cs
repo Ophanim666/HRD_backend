e846f2c14d8d46c44c6666555838e2b0f05462d5
Ophanim666 <102317664+Ophanim666@users.noreply.github.com>
2024-11-19 22:40:36 -0300

    Cambio minimo

--- DTO/Usuario/UsuarioTokenDTO.cs
+++ DTO/Usuario/UsuarioTokenDTO.cs
@@ -10,7 +10,6 @@ namespace DTO.Usuario
     {
         public string Email { get; set; }
         public int EsAdministrador { get; set; }
-        // este id llega nulo si no funciona comentar per es necesario para insertar el usuario en acta
         public int Id { get; set; }
         public string Primer_nombre { get; set; }
     }
